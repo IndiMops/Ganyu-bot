183932da74aac6aa22dd07746b69568d514093f5
IndiMops <bbucko175@gmail.com>
2024-10-21 23:54:57 +0300

    feat: Add menu options for Information and Moderation categories in help command
    This commit adds menu options for the Information and Moderation categories in the help command. Each category has a corresponding description that provides information about the available commands within the category. The menu options allow users to select a category and view detailed information about each command.

--- cogs/Information/help.py
+++ cogs/Information/help.py
@@ -4,6 +4,7 @@ from ganyu_utils import *
 from discord.ext import commands
 from discord import app_commands
 from discord.ui import Select, View
+from datetime import datetime
 
 
 config = LoadJson("config.json")
@@ -25,17 +26,120 @@ class HelpCommand(commands.Cog):
                         label="Інформація",
                         value="1",
                         emoji="📃"
+                    ),
+                    discord.SelectOption(
+                        label="Модерація",
+                        value="2",
+                        emoji="🛡️"
                     )
                 ]
             )
             
             async def callback(interaction: discord.Interaction):
+                # Description of the Information category menu
                 if menu.values[0] == "1":
                     embed = discord.Embed(
-                        title = "Доступні команди категорії 📃Інформація",
-                        descriptions = "Ви можете отримати детальну інформацію для кожної команди, викликавши її за допомогою </{command_name}:{command_id}> `<назва команди чи категорії>`".format(command_name=GetCommand(3)["name"], command_id=GetCommand(3)["id"]),
-                        color=HexToColor(config["color_default"])
+                        title = f"Доступні команди категорії {menu.options[0].emoji} {menu.options[0].label}",
+                        description = "Ви можете отримати детальну інформацію для кожної команди, викликавши її за допомогою </{command_name}:{command_id}> `<command:name>`".format(command_name=GetCommand(3)["name"], command_id=GetCommand(3)["id"]),
+                        color=HexToColor(config["bot"]["color"]["default"])
                     )
+                    embed.set_thumbnail(url = config["bot"]["icon"])
+                    embed.set_footer(
+                        text = "Mops Storage © 2020-{curent_year} Всі права захищено • {dev_site_url}".format(curent_year = datetime.now().year, dev_site_url = config["bot"]["site"]),
+                        icon_url = config["bot"]["icon"]
+                    )
+                    
+                    # Splitting each command of the category via discord.Embed.add_field()
+                    embed.add_field(
+                        name = "• </{0}:{1}>".format(GetCommand(3)["name"], GetCommand(3)["id"]),
+                        value = "↪Відображає перелік усіх доступних команд бота з короткими описами для кожної команди",
+                        inline = False
+                    )
+                    
+                    await interaction.response.edit_message(embed = embed)
+                
+                # Description of the Moderation category menu
+                if menu.values[0] == "2":
+                    embed = discord.Embed(
+                        title = f"Доступні команди категорії {menu.options[1].emoji} {menu.options[1].label}",
+                        description = "Ви можете отримати детальну інформацію для кожної команди, викликавши її за допомогою </{command_name}:{command_id}> `<command:name>`".format(command_name=GetCommand(3)["name"], command_id=GetCommand(3)["id"]),
+                        color=HexToColor(config["bot"]["color"]["default"])
+                    )
+                    embed.set_thumbnail(url = config["bot"]["icon"])
+                    embed.set_footer(
+                        text = "Mops Storage © 2020-{curent_year} Всі права захищено • {dev_site_url}".format(curent_year = datetime.now().year, dev_site_url = config["bot"]["site"]),
+                        icon_url = config["bot"]["icon"]
+                    )
+                    
+                    # Splitting each command of the category via discord.Embed.add_field()
+                    embed.add_field(
+                        name = "• </{0}:{1}>".format(GetCommand(6)["name"], GetCommand(6)["id"]),
+                        value = "↪Виганяє зазначеного учасника з сервера.",
+                        inline = False
+                    )
+                    
+                    embed.add_field(
+                        name = "• </{0}:{1}>".format(GetCommand(7)["name"], GetCommand(7)["id"]),
+                        value = "↪Забороняє доступ зазначеному учаснику на сервер, видаючи йому бан.",
+                        inline = False
+                    )
+                    
+                    embed.add_field(
+                        name = "• </{0}:{1}>".format(GetCommand(8)["name"], GetCommand(8)["id"]),
+                        value = "↪Відображає список учасників, які отримали бан, разом із причинами їх блокування.",
+                        inline = False
+                    )
+                    
+                    embed.add_field(
+                        name = "• </{0}:{1}>".format(GetCommand(9)["name"], GetCommand(9)["id"]),
+                        value = "↪Знімає бан із зазначеного учасника, дозволяючи йому повернутися на сервер.",
+                        inline = False
+                    )
+                    
+                    embed.add_field(
+                        name = "• </{0}:{1}>".format(GetCommand(10)["name"], GetCommand(10)["id"]),
+                        value = "↪Тимчасово позбавляє учасника можливості писати повідомлення на сервері на вказаний період часу (до 28-и діб).",
+                        inline = False
+                    )
+                    
+                    embed.add_field(
+                        name = "• </{0}:{1}>".format(GetCommand(11)["name"], GetCommand(11)["id"]),
+                        value = "↪Знімає тимчасовий м'ют із зазначеного учасника, відновлюючи його можливість писати повідомлення на сервері.",
+                        inline = False
+                    )
+                    
+                    await interaction.response.edit_message(embed = embed)
+
+            # Creating a view and adding the menu to it
+            menu.callback = callback
+            view = View()
+            view.add_item(menu)
+
+            
+            embed = discord.Embed(
+                title = "Доступні команди:",
+                description="Ви можете отримати детальну інформацію для кожної команди, викликавши її за допомогою </{0}:{1}> `<назва команди чи категорії>`. Або ж вибрати нижче категорію команд, щоб переглянути детальну інформацію про кожжну команду із вибраної категорії.".format(GetCommand(4)["name"], GetCommand(4)["id"]),
+                color=HexToColor(config["bot"]["color"]["default"])
+            )
+            
+            embed.set_thumbnail(url = config["bot"]["icon"])
+            embed.set_footer(
+                text = "Mops Storage © 2020-{curent_year} Всі права захищено • {dev_site_url}".format(curent_year = datetime.now().year, dev_site_url = config["bot"]["site"]),
+                icon_url = config["bot"]["icon"]
+            )
+            
+            embed.add_field(
+                name="📃 Інформація (</{command_help_name}:{command_help_id}> `<category:Information>)`".format(command_help_name = GetCommand(4)["name"], command_help_id = GetCommand(4)["id"]),
+                value = "</{0}:{1}>".format(GetCommand(4)["name"], GetCommand(4)["id"]),
+                inline = False
+            )
+            embed.add_field(
+                name="🛡️ Модерація (</{command_help_name}:{command_help_id}> `<category:Moderation`)".format(command_help_name = GetCommand(4)["name"], command_help_id = GetCommand(4)["id"]),
+                value = "</{0}:{1}> </{2}:{3}> </{4}:{5}> </{6}:{7}> </{8}:{9}> </{10}:{11}>".format(GetCommand(6)["name"], GetCommand(6)["id"], GetCommand(7)["name"], GetCommand(7)["id"], GetCommand(8)["name"], GetCommand(8)["id"], GetCommand(9)["name"], GetCommand(9)["id"], GetCommand(10)["name"], GetCommand(10)["id"], GetCommand(11)["name"], GetCommand(11)["id"]),
+                inline = False
+            )
+            
+            await interaction.response.send_message(embed = embed, view = view, ephemeral = True)
         
 
 
